92859d0fc9dac01b6a7db9568d79638fd32dbc47
WilliamYYC <605233050@qq.com>
2020-05-16 11:03:13 +0800

    add UI

---
+++ Src/Server/GameServer/GameServer/Services/HelloWorldServices.cs
@@ -0,0 +1,35 @@
+﻿using Common;
+using Network;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Services
+{
+    class HelloWorldServices:Singleton<HelloWorldServices>
+    {
+        public void Init()
+        {
+           
+        }
+        public void Start()
+        {
+            MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<FirstDefineRequest>(this.onFirstDefineRequest);
+        }
+
+
+        void onFirstDefineRequest(NetConnection<NetSession> sender , FirstDefineRequest firstDefineRequest)
+        {
+            Log.InfoFormat("FirstDefineRequest : {0}", firstDefineRequest.Msg);
+        }
+
+
+        public void Stop()
+        {
+
+        }
+    }
+}
